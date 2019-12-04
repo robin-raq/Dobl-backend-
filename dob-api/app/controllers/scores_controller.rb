96837e374a0ae87209a13062dad438dd0ae39631
robin-raq <robin.raq@gmail.com>
2019-12-04 16:37:14 -0500

    scores create action

--- dob-api/app/controllers/scores_controller.rb
+++ dob-api/app/controllers/scores_controller.rb
@@ -4,5 +4,15 @@ class ScoresController < ApplicationController
         @scores = Score.all
         render json: @scores, include: "**"
     end
+    def create
+        @score = Score.create(score_params)
+        render json: @score
+    end
+
+    private
+
+    def score_params
+        params.permit(:name, :value, :reward_id)
+    end
 
 end
